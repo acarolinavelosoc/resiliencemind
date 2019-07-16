c946bae4343aadb7b53bb1e2c3b3cb788f704db4
acarolinavelosoc <52975302+acarolinavelosoc@users.noreply.github.com>
2019-07-16 15:35:49 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<title>RESILIENCE MIND</title>
+<meta charset="utf-8">
+<link type="text/css" rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat&display=swap">
+</head>
+<body>	
+<header>
+<img src="img/rap.png" class="responsiva">
+<nav id="menu">
+      <ul>
+          <li><a href="index.php">HOME</a></li>
+          <li><a href="sobrenos.phtml">SOBRE</a></li>
+          </li>
+          <li><a href="comofunciona.phtml">CADASTRE-SE</a></li>
+          <li><a href="doutores.phtml">DOUTORES</a>
+          </li>
+          <li><a href="paciente.phtml">PACIENTE</a>
+          </li>
+          <li><a href="comofunciona.phtml">COMO FUNCIONA</a></li>
+          <li><a href="contato.phtml">CONTATO</a></li>
+      </ul>
+      </header>
+
+  <div id="form">
+ 
+      <div id="login" class="">
+        <form action="" method="post">
+          <h1 class="text-center">Seja bem-vindo!</h1>
+          <div class="wrap-field">
+            <label for="email">Email<span class="req">*</span></label>
+            <input type="email" name="email" id="email" required />
+          </div>
+          <div class="wrap-field">
+            <label for="password">Password<span class="req">*</span></label>
+            <input type="password" name="password" id="password" required />
+          </div>
+          <a href="#" style="float: right;">Forgot Password?</a>
+          <button type="submit" name="login">LOG IN</button>
+        </form>
+      </div>
+
+
+</body>
+</html>
+
+
